4f23adbdf6545ee3dd5c332f9192384fec2bcad9
b4dcat <support@b4dcat.ru>
2024-03-04 18:35:38 +0300

    perf: optimize performance

--- tests/test_app.py
+++ tests/test_app.py
@@ -1517,3 +1517,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def new_feature():
+    '''New feature implementation'''
+    return True
