381102eec29e72efae46bfafc0d200c3a1851c9c
b4dcat <support@b4dcat.ru>
2024-05-30 17:08:07 +0300

    fix: resolve bug in function

--- tests/test_app.py
+++ tests/test_app.py
@@ -2293,3 +2293,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def new_feature():
+    '''New feature implementation'''
+    return True
