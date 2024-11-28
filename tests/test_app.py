4b4ea361eb7576ad92f7134ea0a6c63e1df60dea
b4dcat <support@b4dcat.ru>
2024-11-28 10:12:53 +0300

    test: add unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3801,3 +3801,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
