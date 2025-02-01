41df696554fdd96ff9496180c8eacf384883167f
b4dcat <support@b4dcat.ru>
2025-02-01 14:59:03 +0300

    docs: update documentation

--- tests/test_app.py
+++ tests/test_app.py
@@ -4563,3 +4563,7 @@ def improve_performance():
 def new_feature():
     '''New feature implementation'''
     return True
+
+def improve_performance():
+    '''Performance optimization'''
+    pass
